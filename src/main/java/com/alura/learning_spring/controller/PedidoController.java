fd24373712fd097157b5104b2c20678497450173
andrie.balbino <andrie.anderson1@gmail.com>
2021-11-06 07:52:25 -0300

    Adicionado validação do Formulário de novo pedido

--- src/main/java/com/alura/learning_spring/controller/PedidoController.java
+++ src/main/java/com/alura/learning_spring/controller/PedidoController.java
@@ -1,7 +1,10 @@
 package com.alura.learning_spring.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,20 +14,23 @@ import com.alura.learning_spring.model.Pedido;
 import com.alura.learning_spring.repository.PedidoRepository;
 
 @Controller
-@RequestMapping("/pedido")
+@RequestMapping("pedido")
 public class PedidoController {
 	
 	@Autowired
 	private PedidoRepository pedidoRepository;
 	
 	@GetMapping("formulario")
-	public String formulario() {
+	public String formulario(RequisicaoNovoPedido requisicaoNovoPedido) {
 		return "pedido/form_pedido";
 	}
 
 	@PostMapping("novo")
-	public String novo(RequisicaoNovoPedido requisicao) {
-		Pedido pedido = requisicao.toPedido();
+	public String novo(@Valid RequisicaoNovoPedido requisicaoNovoPedido, BindingResult result) {
+		if(result.hasErrors())
+			return "pedido/form_pedido";
+		
+		Pedido pedido = requisicaoNovoPedido.toPedido();
 		pedidoRepository.save(pedido);
 		
 		return "pedido/form_pedido";
--- src/main/java/com/alura/learning_spring/dto/RequisicaoNovoPedido.java
+++ src/main/java/com/alura/learning_spring/dto/RequisicaoNovoPedido.java
@@ -1,11 +1,18 @@
 package com.alura.learning_spring.dto;
 
+import javax.validation.constraints.NotBlank;
+
 import com.alura.learning_spring.model.Pedido;
 
 public class RequisicaoNovoPedido {
 	
+	@NotBlank
 	private String nomeProduto;
+	
+	@NotBlank
 	private String urlProduto;
+	
+	@NotBlank
 	private String urlImagem;
 	private String descricao;
 	
